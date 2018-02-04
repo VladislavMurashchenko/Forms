4eaa8a48206f85d06defee3ca0ff23abc37c3443
Vladislav <vlad.murashchenko@gmail.com>
2018-02-05 01:24:06 +0200

    Add propTypes and controller

--- src/AppForm/AppForm.js
+++ src/AppForm/AppForm.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import getObjectById from '../functions/get-object-by-id';
+
 import './AppForm.css';
 
-export default class AppForm extends Component {
+class AppForm extends Component {
   render() {
     return (
-      <div className="app-form">form</div>
+      <form className="app-form">
+
+      </form>
     );
   }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.id;
+
+  const data = getObjectById(state.formsInfo, id).data;
+
+  console.log(data);
+
+  return {
+    id,
+    data
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppForm);
+
+AppForm.propTypes = {
+  id: PropTypes.number,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.oneOf([
+        'string',
+        'integer'
+      ]),
+      label: PropTypes.string,
+      value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.bool
+      ]),
+      values: PropTypes.arrayOf(PropTypes.string)
+    })
+  )
 }
