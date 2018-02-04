582b4f11dee258256b4b5624bd9be66c3f7b3add
Vladislav <vlad.murashchenko@gmail.com>
2018-02-04 20:03:38 +0200

    mock AppForm component

---
+++ src/AppForm/AppForm.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './AppForm.css';
+
+export default class AppForm extends Component {
+  render() {
+    return (
+      <div className="app-form">form</div>
+    );
+  }
+}
