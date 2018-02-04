e1c161e09c957e215f2eb86ec982b19b8ddbdfda
Vladislav <vlad.murashchenko@gmail.com>
2018-02-04 05:32:07 +0200

    App structure changed

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { Router, Route, browserHistory, Redirect, IndexRedirect } from 'react-router';
+import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
 import './index.css';
+import 'normalize.css';
 import registerServiceWorker from './registerServiceWorker';
-// Components
-import { RouteHome } from './RouteHome';
-import { RouteForm } from './RouteForm';
+import { App } from './App/App';
 
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
@@ -21,15 +20,7 @@ const history = syncHistoryWithStore(browserHistory , store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <Route path="/">
-        <IndexRedirect to='forms' />
-        <Route path="forms/:id" component={RouteForm} />
-        <Route path="forms" component={RouteHome} />
-        <Redirect from="*" to="forms" />
-        <Redirect from="*/*/**" to="forms" />
-      </Route>
-    </Router>
+    <App history={history}/>
   </Provider>,
   document.getElementById('root')
 );
