9d5252940b810af9bc8270e678033a1891057eec
Vladislav <vlad.murashchenko@gmail.com>
2018-02-04 20:02:38 +0200

    beautified code

--- src/FormInfo/FormInfo.js
+++ src/FormInfo/FormInfo.js
@@ -6,13 +6,17 @@ import './FormInfo.css';
 
 export const FormInfo = ( {id, name, description, image, currentPath} ) => {
   const path = `${currentPath}/${id}`;
+  
   return (
     <li className="form-info">
       <div className="form-info__image-wrapper">
         <img src={image} alt={name} className="form-info__image"/>
       </div>
+
       <h2 className="form-info__name">{name}</h2>
+
       <p className="form-info__description">{description}</p>
+
       <div className="form-info__btn-wrapper">
         <button className="form-info__btn">Delete</button>
         <Link to={path} className="form-info__btn">View</Link>
