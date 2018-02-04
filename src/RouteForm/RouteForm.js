9d666b2e451ff77c83d2789fe14356da24fa4a0f
Vladislav <vlad.murashchenko@gmail.com>
2018-02-04 20:32:31 +0200

    + RouteForm component namespace

--- src/RouteForm/RouteForm.js
+++ src/RouteForm/RouteForm.js
@@ -7,21 +7,25 @@ import AppForm from '../AppForm/AppForm';
 class RouteForm extends Component {
   render() {
     const {name, description, image, id, router } = this.props;
-    console.log(router);
+
     return (
       <main className="route-form">
         <section className="route-form__container">
+
           <header className="route-form__header">
-            <div>
-              <img src={image} alt={name}/>
+            <div className="route-form__image-wrapper">
+              <img className="route-form__image" src={image} alt={name}/>
             </div>
-            <h1>{name}</h1>
-            <p>{description}</p>
+            <h1 className="route-form__name">{name}</h1>
+            <p className="route-form__descrition">{description}</p>
           </header>
+
           <AppForm id={id} />
-          <footer>
-            <button onClick={router.goBack}>Go Back</button>
+
+          <footer className="route-form__footer">
+            <button className="route-form__go-back" onClick={router.goBack}>Go Back</button>
           </footer>
+
         </section>
       </main>
     );
